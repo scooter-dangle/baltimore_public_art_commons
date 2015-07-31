541dbd1d309b22b1766c8c13921c588596acb6d7
Maggie Epps <maggie.epps@gmail.com>
2015-07-31 15:37:48 -0400

    Added successful user creation to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    @user.save
     redirect_to new_user_path
   end
 
+  private
+  def user_params
+    params.require(:user).permit(:email, :role)
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe UsersController, type: :controller do
-  let(:admin) {create :user}
+  let(:user) {create :admin}
   describe 'POST :create' do
     it 'should respond with success' do
-      post :create, email: user.email, role: user.role
-      expect(response.status).to eq(200)
+      post :create, user: {email: user.email, role: user.role}
+      expect(response.status).to eq(302)
+    end
+
+    it 'should increase the user count by 1' do
+      expect{
+        post :create, user: {email: user.email, role: user.role}
+      }.to change(User, :count).by 1
     end
   end
 end
